e26f21d536c2ae0417239de566f4e745dcf3810a
M3d15 <0d13rn@gmail.com>
2022-09-09 16:57:14 +0300

    login_page.py added

---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+    
+    def should_be_login_url(self):
+        current_url = self.browser.current_url
+        
+        assert "login" in current_url, "It isn't a login link!"
+
+    def should_be_login_form(self):
+        login_form = self.browser.find_element(*LoginPageLocators.LOGIN_FORM)
+
+        assert login_form, "Login form is not presented!"
+
+    def should_be_register_form(self):
+        register_form = self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
+
+        assert register_form, "Registration form is not presented!"
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,9 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
+"""
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
@@ -14,3 +15,23 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+"""
+
+def test_guest_can_see_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_guest_can_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_can_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+    
